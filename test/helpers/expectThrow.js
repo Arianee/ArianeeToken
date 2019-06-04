8463b9ff09c1bbd1708c404791d2d42880ceb18e
Maxime <maxime@vaugam.com>
2019-06-04 11:39:30 +0200

    remove old tests

--- test/helpers/expectThrow.js
+++
@@ -1,9 +0,0 @@
-export default async (promise, errorName) => {
-  try {
-    await promise;
-  } catch (error) {
-    assert(error.name == errorName, 'Unexpected error name: ' + error.name);
-    return;
-  }
-  assert.fail('Expected throw not received');
-};
